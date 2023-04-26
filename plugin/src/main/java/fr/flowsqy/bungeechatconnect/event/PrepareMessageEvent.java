a67ab0609e9fbd2e991b72b175242dd2aa0693f8
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-04-26 21:04:50 +0200

    Add extra data in PrepareMessageEvent

--- plugin/src/main/java/fr/flowsqy/bungeechatconnect/event/PrepareMessageEvent.java
+++ plugin/src/main/java/fr/flowsqy/bungeechatconnect/event/PrepareMessageEvent.java
@@ -4,7 +4,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Called when the plugin is about to send a message through the network
@@ -17,12 +22,14 @@ public class PrepareMessageEvent extends Event implements Cancellable {
     private final Player player;
     private String format;
     private String message;
+    private final List<ExtraData> extraDataList;
 
     public PrepareMessageEvent(boolean isAsync, @NotNull Player player, @NotNull String format, @NotNull String message) {
         super(isAsync);
         this.player = player;
         this.format = format;
         this.message = message;
+        extraDataList = new LinkedList<>();
     }
 
     @Override
@@ -58,6 +65,15 @@ public class PrepareMessageEvent extends Event implements Cancellable {
         this.message = message;
     }
 
+    public void addExtraData(@NotNull Plugin plugin, byte @NotNull [] data) {
+        extraDataList.add(new ExtraData(plugin, data));
+    }
+
+    @NotNull
+    public List<ExtraData> getExtraDataList() {
+        return Collections.unmodifiableList(extraDataList);
+    }
+
     @NotNull
     @Override
     public HandlerList getHandlers() {
@@ -66,6 +82,26 @@ public class PrepareMessageEvent extends Event implements Cancellable {
 
     public static HandlerList getHandlerList() {
         return handlers;
+    }
+
+    public final static class ExtraData {
+
+        private final Plugin plugin;
+        private final byte[] data;
+
+        public ExtraData(@NotNull Plugin plugin, byte @NotNull [] data) {
+            this.plugin = plugin;
+            this.data = data;
+        }
+
+        @NotNull
+        public Plugin getPlugin() {
+            return plugin;
+        }
+
+        public byte @NotNull [] getData() {
+            return data;
+        }
     }
 
 }
