5f55335e96f6ed103a839f7e34798bf746f64ff1
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-04-26 21:20:06 +0200

    Add PacketWriter

---
+++ plugin/src/main/java/fr/flowsqy/bungeechatconnect/PacketWriter.java
@@ -0,0 +1,25 @@
+package fr.flowsqy.bungeechatconnect;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class PacketWriter {
+
+    private final static String CHANNEL = "BungeeChatConnect";
+
+    public byte @NotNull [] write(@NotNull String serverTo, byte @NotNull [] messageData) throws IOException {
+        final ByteArrayOutputStream outByteStream = new ByteArrayOutputStream();
+        final DataOutputStream outDataStream = new DataOutputStream(outByteStream);
+        outDataStream.writeUTF("Forward");
+        outDataStream.writeUTF(serverTo);
+        outDataStream.writeUTF(CHANNEL);
+        outDataStream.writeShort(messageData.length);
+        outDataStream.write(messageData);
+        outDataStream.flush();
+        return outByteStream.toByteArray();
+    }
+
+}
