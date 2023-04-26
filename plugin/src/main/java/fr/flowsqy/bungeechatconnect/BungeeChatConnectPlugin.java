5c294a2b0d9682c5598c2de4b4cdf491eba9efb5
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-04-26 14:13:37 +0200

    Add main class

---
+++ plugin/src/main/java/fr/flowsqy/bungeechatconnect/BungeeChatConnectPlugin.java
@@ -0,0 +1,11 @@
+package fr.flowsqy.bungeechatconnect;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BungeeChatConnectPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
