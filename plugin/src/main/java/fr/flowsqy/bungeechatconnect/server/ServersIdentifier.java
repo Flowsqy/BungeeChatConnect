134d0fc314d2e058ac7cc453203705cfab4bd364
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-04-27 22:59:26 +0200

    Add ServersIdentifier and ServersQuery

---
+++ plugin/src/main/java/fr/flowsqy/bungeechatconnect/server/ServersIdentifier.java
@@ -0,0 +1,39 @@
+package fr.flowsqy.bungeechatconnect.server;
+
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+public class ServersIdentifier {
+
+    private String[] servers;
+    private ServersQuery serversQuery;
+    private volatile boolean initialized;
+
+    public ServersIdentifier(@NotNull ServersQuery serversQuery) {
+        this.servers = null;
+        this.serversQuery = serversQuery;
+    }
+
+    @NotNull
+    public String[] query(Player player) {
+        if (initialized) {
+            return servers;
+        }
+        synchronized (this) {
+            if (initialized) {
+                return servers;
+            }
+            try {
+                servers = serversQuery.query(player);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            serversQuery = null;
+            initialized = true;
+            return servers;
+        }
+    }
+
+}
