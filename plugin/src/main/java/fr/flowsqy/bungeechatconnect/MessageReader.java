534680aa17eedcc38e4a1543917111f8e54cf1f6
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-04-27 03:01:42 +0200

    Add MessageReader

---
+++ plugin/src/main/java/fr/flowsqy/bungeechatconnect/MessageReader.java
@@ -0,0 +1,31 @@
+package fr.flowsqy.bungeechatconnect;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MessageReader {
+
+    @NotNull
+    public ReceivedMessage read(byte @NotNull [] messageData) throws IOException {
+        final DataInputStream inDataStream = new DataInputStream(new ByteArrayInputStream(messageData));
+        final String name = inDataStream.readUTF();
+        final String displayName = inDataStream.readUTF();
+        final String format = inDataStream.readUTF();
+        final String message = inDataStream.readUTF();
+        final byte extraDataNumber = inDataStream.readByte();
+        final Map<String, byte[]> extraData = new HashMap<>(extraDataNumber);
+        for (int i = 0; i < extraDataNumber; i++) {
+            final String pluginName = inDataStream.readUTF();
+            final byte[] pluginExtraData = new byte[inDataStream.readShort()];
+            inDataStream.readFully(pluginExtraData);
+            extraData.put(pluginName, pluginExtraData);
+        }
+        return new ReceivedMessage(name, displayName, format, message, extraData);
+    }
+
+}
