f3410d025bcb95700acbbe0eddd2c8ff3de0ea60
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-04-27 03:15:52 +0200

    Add MessageSender

---
+++ plugin/src/main/java/fr/flowsqy/bungeechatconnect/MessageSender.java
@@ -0,0 +1,16 @@
+package fr.flowsqy.bungeechatconnect;
+
+import fr.flowsqy.bungeechatconnect.event.BungeePlayerChatEvent;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class MessageSender {
+
+    public void sendMessage(@NotNull BungeePlayerChatEvent event) {
+        final String message = String.format(event.getFormat(), event.getDisplayName(), event.getMessage());
+        for (Player player : event.getRecipient()) {
+            player.sendMessage(message);
+        }
+    }
+
+}
