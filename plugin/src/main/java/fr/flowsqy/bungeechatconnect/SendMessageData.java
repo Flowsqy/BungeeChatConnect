a40c7a1034de8bc21275cad8e8e09a8dbfb8ae1f
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-04-28 02:35:36 +0200

    Add SendMessageData

---
+++ plugin/src/main/java/fr/flowsqy/bungeechatconnect/SendMessageData.java
@@ -0,0 +1,35 @@
+package fr.flowsqy.bungeechatconnect;
+
+import fr.flowsqy.bungeechatconnect.event.PrepareMessageEvent;
+
+import java.util.List;
+
+public class SendMessageData {
+
+    private final String format;
+    private final String message;
+    private final List<PrepareMessageEvent.ExtraData> extraDataList;
+
+    public SendMessageData(String format, String message, List<PrepareMessageEvent.ExtraData> extraDataList) {
+        this.format = format;
+        this.message = message;
+        this.extraDataList = extraDataList;
+    }
+
+    public static SendMessageData snapshot(PrepareMessageEvent event) {
+        return new SendMessageData(event.getFormat(), event.getMessage(), event.getExtraDataList());
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public List<PrepareMessageEvent.ExtraData> getExtraDataList() {
+        return extraDataList;
+    }
+
+}
